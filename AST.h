d91790facfd8aaba3fb6e82afd7c2fd479197bb7
unknown <434446127@qq.com.com>
2023-05-21 17:18:05 +0800

    完善ast.h

--- AST.h
+++ AST.h
@@ -49,13 +49,13 @@ public:
     };
 };
 
-class Identifier : public ExpNode{
+class IdentifierNode : public ExpNode{
 public:
     Identifier(string *name) : name(name){}
     string getname(){return *name;}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
-private:
+
     string *name;
 };
 class IntNode : public Const{
@@ -70,7 +70,6 @@ public:
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
     int value;
 };
 class CharNode : public Const{
@@ -85,39 +84,37 @@ public:
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
     char value;
 };
-class BoolNode : public Const{
-public:
-    BoolNode(bool value) : value(value){}
-    virtual C_TYPE getType(){ return C_BOOLEAN;}
-    virtual Const::Value getValue(){
-        Const::Value v;
-        v.b = value;
-        return v; 
-    }
-    virtual string genJson();
-    virtual llvm::Value *genCode(CodeGenerator & gen);
+// class BoolNode : public Const{
+// public:
+//     BoolNode(bool value) : value(value){}
+//     virtual C_TYPE getType(){ return C_BOOLEAN;}
+//     virtual Const::Value getValue(){
+//         Const::Value v;
+//         v.b = value;
+//         return v; 
+//     }
+//     virtual string genJson();
+//     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
-    bool value;
-};
-class FloatNode : public Const{
-public:
-    FloatNode(float value) : value(value){}
-    virtual C_TYPE getType(){ return C_FLOAT;}
-    virtual Const::Value getValue(){
-        Const::Value v;
-        v.f = value;
-        return v; 
-    }
-    virtual string genJson();
-    virtual llvm::Value *genCode(CodeGenerator & gen);
+//     bool value;
+// };
+// class FloatNode : public Const{
+// public:
+//     FloatNode(float value) : value(value){}
+//     virtual C_TYPE getType(){ return C_FLOAT;}
+//     virtual Const::Value getValue(){
+//         Const::Value v;
+//         v.f = value;
+//         return v; 
+//     }
+//     virtual string genJson();
+//     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
-    float value;
-};
+// private:
+//     float value;
+// };
 
 class DoubleNode : public Const{
 public:
@@ -131,75 +128,101 @@ public:
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
     double value;
 };
 
 class StringNode : public ExpNode{
 public:
-    StringNode(string &value):value(value){}
+    StringNode(string *value):value(value){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
 
-private:
-    string &value;
+    string *value;
 };
 class ArrayEleNode : public ExpNode{
 public:
-    ArrayEleNode(){}
+    ArrayEleNode(IdentifierNode* _identifier,ExpNode* _index):identifier(_identifier),index(_index){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+    
+    IdentifierNode * identifier;
+    ExpNode* index;
 };
 
 class ArrayAssNode : public ExpNode{
 public:
-    ArrayAssNode(){}
+    ArrayAssNode(IdentifierNode* _identifier,ExpNode* _index,ExpNode* _rhs):identifier(_identifier),index(_index),rhs(__rhs){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode * identifier;
+    ExpNode* index;
+    ExpNode* rhs; 
     
 };
 class FunCallNode : public ExpNode{
 public:
-    FunCallNode(){}
+    FunCallNode(IdentifierNode *_identifier, vector<ExpNode*> _args) : identifier(_identifier), args(_args) {}
+    FunCallNode(IdentifierNode *_identifier) :  identifier(identifier) {}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode * identifier;
+    vector<ExpNode*> args;
+
 };
 class BinOpNode : public ExpNode{
 public:
-    BinOpNode(){}
+    BinOpNode(int _op,ExpNode* _l,ExpNode* _r):op(_op),l(_l),r(_r){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+    int op;
+    ExpNode* l;
+    ExpNode* r;
 };
 class getAddrNode : public ExpNode{
 public:
-    getAddrNode(){}
+    getAddrNode(IdentifierNode *_identifier):identifier(_identifier){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode *identifier;
 };
 class getArrayAddrNode : public ExpNode{
 public:
-    getArrayAddrNode(){}
+    getArrayAddrNode(IdentifierNode *_identifier, ExpNode *_index) :index(_index), identifier(_identifier) {}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    ExpNode* index;
+    IdentifierNode *identifier;
 };
 class AssignNode : public ExpNode{
 public:
-    AssignNode(){}
+    AssignNode(IdentifierNode *_identifier, ExpNode *_index) :lhs(_index), identifier(_identifier) {}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode *identifier;
+    ExpNode *lhs;
 };
 class BlockNode : public ExpNode{
 public:
     BlockNode(){}
+    BlockNode(vector<StmNode*> _stmlist):stmlist(_stmlist){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    vector<StmNode*> stmlist;
 };
 
 class ExpStmNode : public StmNode{
 public:
-    ExpStmNode(){}
+    ExpStmNode(ExpNode* _exp):exp(_exp){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    ExpNode* exp;
 };
 class BreakStmNode : public StmNode{
 public:
@@ -209,50 +232,75 @@ public:
 };
 class IfStmNode : public StmNode{
 public:
-    IfStmNode(){}
+    IfStmNode(ExpNode* _exp,BlockNode*_block):exp(_exp),block(_block){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    ExpNode *exp;
+    BlockNode *block;
 };
 class IfElseStmNode : public StmNode{
 public:
-    IfElseStmNode(){}
+    IfElseStmNode(ExpNode* _exp,BlockNode*_block,BlockNode*block_):exp(_exp),ifblock(_block),elseblock(block_){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    ExpNode *exp;
+    BlockNode *ifblock,*elseblock;
 };
 class WhileStmNode : public StmNode{
 public:
-    WhileStmNode(){}
+    WhileStmNode(ExpNode* _exp,BlockNode*_block):exp(_exp),block(_block){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+    ExpNode *exp;
+    BlockNode *block;
 };
 class ForStmNode : public StmNode{
 public:
-    ForStmNode(){}
+    ForStmNode(ExpNode* _exp,BlockNode*_block):exp(_exp),block(_block){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+    ExpNode *exp;
+    BlockNode *block;
 }
 class ReturnNullStmNode : public StmNode{
 public:
     ReturnNullStmNode(){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
 };
 class ReturnStmNode : public StmNode{
 public:
-    ReturnStmNode(){}
+    ReturnStmNode(ExpNode* _exp):exp(_exp){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    ExpNode *exp;
 };
 class VarDecNode : public StmNode{
 public:
     VarDecNode(){}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode *type;
+    IdentifierNode *identifier;
+    ExpNode* exp;
+    int size;
+
 };
 class FunDecNode : public StmNode{
 public:
-    FunDecNode(){}
+    FunDecNode(IdentifierNode *_type, IdentifierNode *_identifier, 
+    vector<VarDecNode*> _args, BlockNode *_block) :  type(_type), identifier(_identifier), args(_args), block(_block) {}
     virtual string genJson();
     virtual llvm::Value *genCode(CodeGenerator & gen);
+
+    IdentifierNode *type;
+    IdentifierNode *identifier;
+    BlockNode *block;
+    vector<VarDecNode*> args;
 };
 #endif
